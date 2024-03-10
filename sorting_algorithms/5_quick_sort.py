9800ff7ee5092ee7063a9179c39fc81c28f8dd9a
MeyiGi <130828110+MeyiGi@users.noreply.github.com>
2024-03-10 11:10:34 +0600

    Update 5_quick_sort.py

--- sorting_algorithms/5_quick_sort.py
+++ sorting_algorithms/5_quick_sort.py
@@ -1,3 +1,32 @@
-s = "Hell"
-left = 3
-s = s[left:]
+import numpy as np
+from typing import List
+
+def quick_sort(arr: List[int], left: int, right: int) -> None:
+    if left < right:
+        partision_pos = partition(arr, left, right)
+        quick_sort(arr, left, partision_pos - 1)
+        quick_sort(arr, partision_pos + 1, right)
+    
+def partition(arr: List[int], left: int, right: int) -> None: 
+    i = left
+    j = right - 1
+    pivot = arr[right]
+    
+    while i < j:
+        while i < right and arr[i] < pivot:
+            i += 1
+        while j > left and arr[j] >= pivot:
+            j -= 1
+            
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    if arr[i] > pivot:
+        arr[i], arr[right] = arr[right], arr[i]
+        
+    return i
+        
+        
+
+lst = np.random.permutation(np.arange(1, 20))
+print("unsorted list: ", lst)
+quick_sort(lst, 0, len(lst) - 1)
