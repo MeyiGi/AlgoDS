9e4ee53063fdfb56ba8dbc2d3f6db44279de163e
MeyiGi <130828110+MeyiGi@users.noreply.github.com>
2024-03-18 03:13:01 +0600

    Update binary_search.py

--- BSA/binary_search.py
+++ BSA/binary_search.py
@@ -1,18 +1,29 @@
-target = 15
-lst = [1, 2, 2, 2, 4, 5, 5, 6, 8, 9, 10, 10, 11, 12, 13, 13, 16, 17, 18, 19]
+from typing import List
 
-def find_num(lst, target):
-    start, end = 0, len(lst) - 1
+def binary_search(arr: List[int], target: int) -> int:
+    """Binary search on sorted array"""
+    if not arr:
+        return
     
-    while start <= end:
-        mid = (start + end) // 2
-        if lst[mid] == target:
-            return f"Founded num {target} at {mid} index"
-        elif lst[mid] > target:
-            end = mid - 1
+    left: int = 0
+    right: int = len(arr) - 1
+    
+    while left <= right:
+        mid: int = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
         else:
-            start = mid + 1
+            right = mid - 1
             
-    return "unfortunatly there are no such num"
+    return -1
 
-print(find_num(lst, target))
+def main() -> None:
+    """Main function"""
+    target: int = 13
+    lst: List[int] = [1, 2, 2, 2, 4, 5, 5, 6, 8, 9, 10, 10, 11, 12, 13, 13, 16, 17, 18, 19]
+    print(binary_search(lst, target))
+    
+if __name__ == "__main__":
+    main()
